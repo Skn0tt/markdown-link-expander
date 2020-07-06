6af6f3d493e5efd9434e7a254aa4cfa9b7d1e928
Simon Knott <info@simonknott.de>
2020-07-06 15:00:13 +0200

    Implement extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { fetchTitle } from "./test/fetchTitle";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "markdown-link-expander.expand",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-link-expander" is now active!');
+      const document = editor.document;
+      const selection = editor.selection;
+			let url = document.getText(selection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-link-expander.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+			if (!url.startsWith("http")) {
+				url = "https://" + url;
+			}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from markdown-link-expander!');
-	});
+      await vscode.window.withProgress(
+        { location: vscode.ProgressLocation.Notification, title: "Fetching title ..." },
+        async () => {
+          try {
+            const title = await fetchTitle(url);
+            const markdownLink = `[${title}](${url})`;
+            editor.edit((editBuilder) => {
+              editBuilder.replace(selection, markdownLink);
+            });
+          } catch (error) {
+            vscode.window.showErrorMessage("Failed to fetch title.");
+          }
+        }
+      );
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/test/fetchTitle.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export async function fetchTitle(url: string): Promise<string> {
+  const res = await axios.get<string>(url);
+  const $ = cheerio.load(res.data);
+  const title = $("title").text();
+  return title;
+}
